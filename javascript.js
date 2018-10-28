c64187d0d83e73084b1b44a1cd41164c74bb525f
jemiah <jemiah11@hotmail.com>
2018-10-28 22:48:30 +0100

    verbeteringen
    -Verhalen schalen mee met de content

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
+var downloadKnop = document.querySelector('footer button');
+var mijnLijstNotificatie = document.classList('.melding');
+
+downloadKnop.addEventListener('click', function () {
+    downloadKnop.classList.toggle('gedownload');
+});
+mijnLijstNotificatie.addEventListener('click', function () {
+    downloadKnop.classList.toggle('.melding');
+});
+
+
+var kruisjes = document.querySelector('footer button');
+
+
+document.querySelector('#knop').addEventListener('click', meme, false);
