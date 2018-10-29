664076dc95172bb968368711885ab4e62bd800fc
jemiah <jemiah11@hotmail.com>
2018-10-29 23:28:51 +0100

    verbeteringen
    verhaal geschaald

--- javascript.js
+++ javascript.js
@@ -1,18 +1,27 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
-var downloadKnop = document.querySelector('footer button');
-var mijnLijstNotificatie = document.classList('.melding');
 
+var downloadKnop = document.querySelector('footer button');
+//console.log("downloadKnop", downloadKnop);
 downloadKnop.addEventListener('click', function () {
+    //console.log("downloadKnop", downloadKnop);
     downloadKnop.classList.toggle('gedownload');
 });
+
+
+
+
+var mijnLijstNotificatie = document.classList('.melding');
+
+
+
 mijnLijstNotificatie.addEventListener('click', function () {
     downloadKnop.classList.toggle('.melding');
 });
+//
+
+//var kruisjes = document.querySelector('footer button');
 
 
-var kruisjes = document.querySelector('footer button');
-
-
-document.querySelector('#knop').addEventListener('click', meme, false);
+//document.querySelector('#knop').addEventListener('click', meme, false);
