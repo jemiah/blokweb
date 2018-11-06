619c28d5813e0017077019f520e95073440c278c
jemiah <jemiah11@hotmail.com>
2018-11-06 20:14:27 +0100

    veel verbeteringen
    - empty states
    - tweede micro interaction
    - zoek pagina

--- javascript.js
+++ javascript.js
@@ -4,11 +4,13 @@
 
 var downloadKnop = document.querySelector('footer button');
 var mijnLijstNotificatie = document.querySelector('.melding');
-var deleten = document.querySelector('article>button');
-var verstop = document.querySelector('section div article:last-of-type');
 var bewerken = document.querySelector('form ul li button');
+var deletenEen = document.querySelector('article>button');
+var deletenTwee = document.querySelector('article:nth-of-type(2)>button');
+var verstop = document.querySelector('.verdwijn');
+var form = document.querySelector('nav > ul > li:nth-child(4)');
+var zoek = document.querySelector('.zoek');
 console.log('test');
-
 
 
 downloadKnop.addEventListener('click', function () {
@@ -20,12 +22,19 @@ downloadKnop.addEventListener('click', function () {
 bewerken.addEventListener('click', function (event) {
     event.preventDefault();
     bewerken.classList.toggle('klik');
+    deletenEen.classList.toggle('show');
+    deletenTwee.classList.toggle('show');
     console.log('pastaaaaa');
 });
 
 
-deleten.addEventListener('click', function () {
-    deleten.classList.toggle('kruisje');
-    verstop.classList.toggle('weg');
+deletenTwee.addEventListener('click', function () {
+    verstop.classList.toggle('verwijderen');
+    deletenTwee.classList.toggle('kruisje');
     console.log('rijst');
+});
+
+form.addEventListener('click', function () {
+    zoek.classList.toggle('laatzien');
+    console.log('testa');
 });
